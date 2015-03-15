527103976ceb3fc5db6d08ca29620f1ecb0ccf57
dewald <dewald@dewald-laptop.(none)>
2015-03-16 00:38:36 +0200

    add ruby and npm installers

--- bootstrap_ubuntu12.0.4.sh
+++ bootstrap_ubuntu12.0.4.sh
@@ -5,14 +5,21 @@ mkdir -v ~/work/sites
 
 sudo mv ./vhost-template /etc/apache2/sites-available/[site].dev.conf
 
+# DEVELOPER TOOLS
 sudo apt-get install --assume-yes git
 sudo add-apt-repository --assume-yes ppa:webupd8team/sublime-text-3
 sudo apt-get --assume-yes update
 sudo apt-get install --assume-yes sublime-text-installer
 
+# LAMP
 sudo apt-get install --assume-yes apache2
 sudo apt-get install --assume-yes phpmyadmin
 sudo apt-get install --assume-yes php5-cli
+
+# NODE & RUBY INSTALL
+sudo apt-get install --assume-yes build-essential git-core g++ gcc make libc6-dev patch openssl ca-certificates libreadline6 libreadline6-dev curl zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev autoconf libc6-dev libgdbm-dev libncurses5-dev automake libtool bison pkg-config libffi-dev
+bash ./node.install.sh
+bash ./rb.install.sh
 
 # USER INPUT REQUIRED
 sudo apt-get install mysql-server
