cb7fa00880691be2a69c215b8c1a61bc8a5649bb
dewald <dewald@dewald-laptop.(none)>
2015-03-16 12:41:41 +0200

    edit node install method

--- node.install.sh
+++ node.install.sh
@@ -1,15 +1,6 @@
-cd ~/
-mkdir ~/local
-echo 'export PATH=$HOME/local/bin:$PATH' >> ~/.bashrc
-. ~/.bashrc
-
-mkdir .bash-js
-cd .bash-js
-
-git clone git://github.com/joyent/node.git
-cd node
-git checkout v0.6
-./configure --prefix=~/local
-make install
- 
-sudo apt-get installassume-yes
+sudo apt-add-repository ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get install --assume-yes nodejs
+touch $HOME/.bashrc
+cat prefix = $HOME/.npm
+echo 'export PATH=$HOME/.npm/bin:$PATH' >> ~/.bashrc
